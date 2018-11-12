cfdf218774cdc13139b0ed512d0aeda160173786
Damian Buttle <damianbuttle@gmail.com>
2018-11-11 23:23:27 -0500

    New Title

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ class HomeIndex extends React.Component {
         return (
             <Layout>
                 <Helmet
-                    title="Gatsby Starter - Forty"
+                    title="Damian Buttle - Front End Developer & Web Designer"
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
