91a8917f7c88ff32ef01ba63c17ce9752f5038f4
Damian Buttle <damian.buttle@flexiana.com>
2018-12-31 18:09:36 -0500

    new form

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,11 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Hi, my name is Damian Buttle</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>web developer and designer,<br />
+                Let's work together to make something awesome.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
                 </ul>
