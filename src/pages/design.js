bbbbca6e3db5d9f7c16a133ba816a212232febd9
Damian Buttle <damianbuttle@gmail.com>
2019-03-22 15:22:34 -0400

    fixed error

--- src/pages/design.js
+++ src/pages/design.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
-import Layout from '../components/layout'
-import BannerLanding from '../components/BannerLanding'
+import SubLayout from '../components/sublayout'
 
 import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
